54066572d5eda797e10e798ae9109949f80590ae
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-10-28 17:32:34 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,22 @@
 var titulo = document.querySelector("h1");
-console.log(titulo);
-console.log(titulo.textContent);
-
 titulo.textContent = "Banana";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+if(peso < 0){
+  console.log("Peso inválido");
+}
+
+if(peso > 1000){
+  console.log("Peso inválido");
+}
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
