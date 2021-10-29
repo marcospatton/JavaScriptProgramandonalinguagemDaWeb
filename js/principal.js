77399ec033f9965b7784422413efb75424cc6ded
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-10-29 19:12:54 -0300

    Atualização do arquivo JS
    Atualização do arquivo Js

--- js/principal.js
+++ js/principal.js
@@ -1,30 +1,39 @@
-var titulo = document.querySelector("h1");
-titulo.textContent = "Banana";
+//var titulo = document.querySelector(".titulo");
+//titulo.textContent="Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.lengt; i++) {
 
-var tdImc = paciente.querySelector(".info-imc");
+    var paciente = pacientes[i];
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-if(peso < 0= || peso >= 1000){
-  console.log("Peso inválido");
-  pesoEhValido = false;
-  tdImc.textContent = "peso Inválido";
-}
-if(altura <= 0 || altura >= 3.00){
-  console.log("Altura inválida");
-  alturaEhValida = false;
-  tdImc.textContent ="Altura inválida !";
-}
-if(alturaEhValida && pesoEhValido){
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+      //  tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+      //  tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+  if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
